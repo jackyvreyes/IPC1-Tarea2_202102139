378832ea6c7d9f371a8807da9f1353521c9277fa
Jacky Reyes <jackyvreyes@gmail.com>
2022-09-02 00:16:22 -0600

    Tarea 2

---
+++ src/tarea_2/Tarea_2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea_2;
+
+import Ventanas.Login;
+import Ventanas.inf;
+
+/**
+ *
+ * @author Jacky Reyes
+ */
+public class Tarea_2 {
+public static String user="jackeline";
+    public static String pass="202102139";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //inf i = new inf();
+        // i.setVisible(true);
+        
+          Login l = new Login();
+         l.setVisible(true);
+            
+        
+    }
+    
+}
